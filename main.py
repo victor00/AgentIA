9fa6541b4a43f6ff3f55dc25d4cc92ccd9683fc7
Antonio Morelli <4491145+victor00@users.noreply.github.com>
2026-05-24 20:19:27 -0300

    add: terminal ai with verbose args

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
+import argparse
+
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 
@@ -9,8 +12,44 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 def main():
-    print(f"My API key is: {api_key}")
+    parser = argparse.ArgumentParser(description="Chatbot")
 
+    parser.add_argument(
+        "user_prompt",
+        type=str,
+        help="User prompt"
+    )
+
+    parser.add_argument(
+        "--verbose",
+        action="store_true",
+        help="Enable verbose output"
+    )
+
+    args = parser.parse_args()
+
+    messages: list[types.Content] = [
+        types.Content(
+            role="user",
+            parts=[types.Part(text=args.user_prompt)]
+        )
+    ]
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=messages
+    )
+
+    if response.usage_metadata is None:
+        raise RuntimeError("No usage metadata returned from the API.")
+
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print()
+
+    print(response.text)
 
 if __name__ == "__main__":
     main()
