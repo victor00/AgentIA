161b3240300c95ff8910c43630afa86e482b0b10
Antonio Morelli <4491145+victor00@users.noreply.github.com>
2026-05-24 20:07:26 -0300

    add: env and it to gitignore

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
 def main():
-    print("Hello from agentia!")
+    print(f"My API key is: {api_key}")
 
 
 if __name__ == "__main__":
